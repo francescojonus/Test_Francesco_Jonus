30d06c1267b483b1a948f542d6c99f25d563af15
Francesco Jonus <francescojonus@yahoo.it>
2012-05-22 10:26:37 +0200

    Prima prova con database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,6 +12,23 @@ namespace MvcApplication2.Controllers
         {
             ViewBag.Message = "Benvenuto al test di prova di Francesco Jonus!";
 
+            ViewBag.DatiDatabase = "<br><br>Dati seguenti letti...<br><br>";
+
+            // using (SqlConnection conn = new SqlConnection("Database=Database1;Server=localhost\\SQL2008;Integrated Security=SSPI")) { using (SqlCommand cmd = new SqlCommand("select count(*) from DatiVari", conn)) { conn.Open(); ViewBag.DatiDatabase = "{0}"; cmd.ExecuteScalar()); } }
+
+            // ViewBag.DatiDatabase = "";
+
+            using (DataSet1 northwind = new DataSet1())
+            {
+                using (DataSet1TableAdapters.DatiVariTableAdapter ta = new DataSet1TableAdapters.DatiVariTableAdapter())
+                {
+                    ta.Fill(northwind.DatiVari); foreach (DataSet1.DatiVariRow row in northwind.DatiVari)
+                    {
+                        ViewBag.DatiDatabase = ViewBag.DatiDatabase + row.Nome + " " + row.Cognome + "<br>";
+                    } 
+                } 
+            }
+
             return View();
         }
 
